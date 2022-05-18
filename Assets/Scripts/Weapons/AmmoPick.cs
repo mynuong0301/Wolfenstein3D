d11849504e0bbf4072e18b77b59796d10774f2e9
mynuong0301 <67038749+mynuong0301@users.noreply.github.com>
2022-05-18 22:57:13 +0700

    some objects in house + animation

--- Assets/Scripts/Weapons/AmmoPick.cs
+++ Assets/Scripts/Weapons/AmmoPick.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AmmoPick : MonoBehaviour
 {
     public GameObject ammoBox;
     public AudioSource ammoPickupSound;
+    public GameObject pickUpDisplay;
 
     private IEnumerator OnTriggerEnter(Collider other)
     {
@@ -14,6 +16,9 @@ public class AmmoPick : MonoBehaviour
         yield return new WaitForSeconds(0.8f);
         ammoPickupSound.Play();
         other.GetComponent<GlobalAmmo>().PickupAmmo(10);
+        pickUpDisplay.SetActive(false);
+        pickUpDisplay.GetComponent<Text>().text = "Pick up 10 bullets";
+        pickUpDisplay.SetActive(true);
     }
 
 }
--- Assets/Scripts/Weapons/HandGunPickUp.cs
+++ Assets/Scripts/Weapons/HandGunPickUp.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 public class HandGunPickUp : MonoBehaviour
 {
     public GameObject handGun;
     public GameObject collectableGun;
     public AudioSource gunPickupSound;
+    public GameObject pickUpDisplay;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -15,6 +16,9 @@ public class HandGunPickUp : MonoBehaviour
         collectableGun.SetActive(false);
         gunPickupSound.Play();
         other.GetComponent<GlobalAmmo>().PickupAmmo(5);
+        pickUpDisplay.SetActive(false);
+        pickUpDisplay.GetComponent<Text>().text = "Pick up Gun";
+        pickUpDisplay.SetActive(true);
     }
 
 }
